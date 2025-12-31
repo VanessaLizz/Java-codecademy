c2644aea26d8f8302a1d2bfbbd72d193ecc66343
Vanessa Liz <vanessalizsousa1996@gmail.com>
2025-12-31 11:22:53 -0300

    repositório criado para realizar commits de atividades dos cursos da codecademy

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Let's play hide and seek.");
+        System.out.print("Three...");
+        System.out.print("Two...");
+        System.out.println("One...");
+        System.out.println("Ready or not, here I come!");
+        System.out.print("I found you!!");
+
+        // barras duplas são usadas para comentários simples em uma única linha
+
+        /* barra simples com asterisco no início e no fim
+        são usadas em comentários que
+        utilizam várias linhas em sua escrita
+         */
+
+        /**
+         * o comentário em java doc é utilizado em casos especiais quando há necessidade de explicar o código
+         * @param args serve para explicar o parametro
+         */
+        System.out.println("Java is a class-based language");
+        System.out.println("Java classes have a 'main' method");
+        System.out.println("Java statements end with a semicolon");
+        System.out.println("My first Java program from scratch in english");
+
+        //abaixo temos a criação de uma ávore de natal
+
+        System.out.println("I going to plant a Christmas tree!!!");
+
+        String[] tree = {
+                "     *     ",
+                "    ***    ",
+                "   *****   ",
+                "  *******  ",
+                " ********* ",
+                "     *   ",
+                "     *   "
+        };
+        for (String line : tree) {
+            System.out.println(line);
+        }
+    }
+}
